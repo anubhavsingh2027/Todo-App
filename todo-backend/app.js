08bbfcaa0c00e32a739cd4246d25ed27aba9c8cb
Anubhav Singh <anubhavsingh2027@gmail.com>
2025-09-22 00:26:57 +0530

    Add files via upload

---
+++ todo-backend/app.js
@@ -0,0 +1,43 @@
+//core module
+require('dotenv').config();
+const path=require('path');
+
+//External module
+
+const express=require('express');
+const bodyParser = require('body-parser');
+const cors=require('cors');
+const { default: mongoose } = require('mongoose');
+//==mongo url ==
+const mongoUrl = process.env.MONGO_URI;
+const port = process.env.PORT || 3000;
+
+//Local module
+const rootdir=require('./utils/pathutils');
+const errorcontroler=require("./controllers/error");
+const todoRouter = require('./router/todoItemRouter');
+
+
+//serever create
+const app=express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(rootdir,'public')));
+
+
+
+
+app.use("/api/todo",todoRouter)
+app.use(errorcontroler.errorpage);
+
+
+mongoose.connect(mongoUrl).then(()=>{
+  console.log("<========  mongoDb connect Successfully =======>");
+  app.listen(port,()=>{
+    console.log(`Server Running At http://localhost:${port}`);
+  })
+}).catch(err=>{
+  console.log("Error while conniting mongoDb",err);
+})
